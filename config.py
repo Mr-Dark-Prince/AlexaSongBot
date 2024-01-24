8fbf3be983d1c47ce6f86c1665adfe8902b36ab5
Lakshay3434 <156693916+Lakshay3434@users.noreply.github.com>
2024-01-24 20:41:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+API_ID = int(os.getenv("24301715"))
+API_HASH = os.getenv("cb4f7937fdd67632695f6677aef90197")
+BOT_TOKEN = os.getenv("6425591334:AAF32OS-AsyAe7nYrD7kgAjtjS4SVz3ODiM")
+DATABASE_URL = os.getenv("mongodb+srv://Lakshay3434:QoFQoovNEFpV8hMf@cluster0.gqe5wwk.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = list({int(x) for x in os.environ.get("6612947670", "").split()})
